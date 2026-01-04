94fa9be7818bcfd17b801431212ecb23c78597ab
singh <ra.rahulsingh85@gmail.com>
2026-01-05 02:55:16 +0530

    Playing with router

--- src/app.js
+++ src/app.js
@@ -6,25 +6,10 @@ const app = express();
 // app.use("/test", (req, res) => {
 //   res.send("Hello From the testing department");
 // });
-app.use("/user", (req, res) => {
-  res.send("Hello From the Server");
-});
-
-app.get("/get",(req,res)=>{
-  res.send("get request call successfully")
-})
-app.post("/post",(req,res)=>{
-  res.send("post request call successfully")
-})
-app.delete("/delete",(req,res)=>{
-  res.send("delete request call successfully")
-})
-
-
-app.use("/main", (req, res) => {
-  res.send("Hello From the Server");
-});
-
+ app.get("/user/:userId/:name/:password",(req,res)=>{
+  console.log(req.params)
+  res.send({fistname: "Rahul",lastName:"Singh"})
+ })
 app.listen(7777, () => {
-  console.log("Server is successfully listening on port 7777");
+  console.log("Server is successfully listening on port 77777");
 });
