cdf5d3a46e50af12268180394be4f68f86468a3c
singh <ra.rahulsingh85@gmail.com>
2026-01-05 02:34:37 +0530

    making get,post,delete api

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
-app.use("/test", (req, res) => {
-  res.send("Hello From the testing department");
+// app.use("/", (req, res) => {
+//   res.send("Hello From the dashboard");
+// });
+// app.use("/test", (req, res) => {
+//   res.send("Hello From the testing department");
+// });
+app.use("/user", (req, res) => {
+  res.send("Hello From the Server");
 });
+
+app.get("/get",(req,res)=>{
+  res.send("get request call successfully")
+})
+app.post("/post",(req,res)=>{
+  res.send("post request call successfully")
+})
+app.delete("/delete",(req,res)=>{
+  res.send("delete request call successfully")
+})
+
+
 app.use("/main", (req, res) => {
   res.send("Hello From the Server");
 });
-app.use("/", (req, res) => {
-  res.send("Hello From the dashboard");
-});
-app.listen(3333, () => {
-  console.log("Server is successfully listening on port 7000");
+
+app.listen(7777, () => {
+  console.log("Server is successfully listening on port 7777");
 });
