3b149fec1482beaaa3e987968cc096602e0f3e8e
singh <ra.rahulsingh85@gmail.com>
2026-01-06 02:22:15 +0530

    Mutiple routes handlers

--- src/app.js
+++ src/app.js
@@ -6,10 +6,34 @@ const app = express();
 // app.use("/test", (req, res) => {
 //   res.send("Hello From the testing department");
 // });
- app.get("/user/:userId/:name/:password",(req,res)=>{
-  console.log(req.params)
-  res.send({fistname: "Rahul",lastName:"Singh"})
- })
+//  app.get("/user/:userId/:name/:password",(req,res)=>{
+//   console.log(req.params)
+//   res.send({fistname: "Rahul",lastName:"Singh"})
+//  })
+app.get("/user", (req, res,next) => {
+  console.log("kya ji user 11 ");
+  // res.send("hello from the user 1");
+  next();
+  
+},
+(req,res,next)=>{
+  console.log("kya ji user 2 ");
+  //res.send("hello from the res 2")
+  next()
+
+},
+(req,res,next)=>{
+  //res.send("hello from the res 2")
+  console.log("kya ji user 3 ");
+  next()
+
+},
+(req,res,next)=>{
+  console.log("kya ji user 4 ");
+  res.send("hello from the res 4")
+
+}
+);
 app.listen(7777, () => {
   console.log("Server is successfully listening on port 77777");
 });
