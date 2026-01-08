7e9ddb068f82cea962dd124a449dbbee59e74c01
singh <ra.rahulsingh85@gmail.com>
2026-01-08 11:41:35 +0530

    add validation

--- src/model/user.js
+++ src/model/user.js
@@ -1,19 +1,34 @@
 const mongoose = require("mongoose");
+const {Schema}=mongoose;
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
+    required:true,
   },
   lastName: {
     type: String,
   },
   emailId: {
     type: String,
+    required:true,
+    unique:true,
   },
   password: {
     type: Number,
+    required:true,
   },
   age: {
     type: String,
+  },
+  photurl:{
+    type:String,
+  },
+  about:{
+    type:String,
+    default:"This is a default about of the user"
+  },
+  skills:{
+    type:[String],
   },
 });
 const userModel = mongoose.model("user", userSchema);
